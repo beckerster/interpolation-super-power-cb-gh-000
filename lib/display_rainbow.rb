91bbdcac920ca9eb07a96cf2f50dac923b37e719
Matt Boecker <matt.boecker@becktronics.org>
2018-04-07 00:40:19 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
-def display_rainbow(colors)
-  colors = ["R:","O:","Y:","G:","B:","I:","V:"]
+def display_rainbow
+  colors = ["R: red","O: orange","Y: yellow","G: green","B: blue","I: indigo","V: violet"]
 puts " #{colors[0]}, #{colors[1]}, #{colors[2]}, #{colors[3]}, #{colors[4]}, #{colors[5]}, #{colors[6]}"
 end
